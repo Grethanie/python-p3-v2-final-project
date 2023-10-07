415c4fd7c6d4d0a2c94f65ccb3bcd0a0fc5ff2a1
Daniel Young <Danyoung91@gmail.com>
2023-10-06 20:36:45 -0400

    band setters created

--- lib/models/band.py
+++ lib/models/band.py
@@ -4,3 +4,10 @@ class Band:
     def get_name(self): return self._name
     def get_albums(self): return self._albums
     def get_genre(self): return self._genre
+    
+    def set_name(self, name):
+        self._name = name
+    def set_albums(self, albums):
+        self._albums = albums
+    def set_genre(self, genre):
+        self._genre = genre
