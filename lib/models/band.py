1fb460ab5bd2008a461ab06b126eaf84b64d0fa8
Daniel Young <Danyoung91@gmail.com>
2023-10-06 20:32:54 -0400

    band getters created

--- lib/models/band.py
+++ lib/models/band.py
@@ -1,4 +1,6 @@
 class Band:
     def __init__(self, name, albums, genre):
         self.name, self.albums, self.genre = name, albums, genre
-        
+    def get_name(self): return self._name
+    def get_albums(self): return self._albums
+    def get_genre(self): return self._genre
