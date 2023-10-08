605657777590b6cc0043edf8fdba56bc74f5eba3
Daniel Young <Danyoung91@gmail.com>
2023-10-08 11:50:00 -0400

    added db functions to band

--- lib/models/band.py
+++ lib/models/band.py
@@ -1,24 +1,76 @@
+from __init__ import CURSOR, CONN
 from album import Album
 from song import Song
 class Band:
-    all = []
+    all = {}
     
-    def __init__(self, name, genre):
-        self.name, self.genre = name, genre
-        self.albums = [album for album in Album.all if album.band == self]
-        self.songs = [song for song in Song.all if song.band == self]
+    def __init__(self, name, genre, id=None):
+        self.name, self.genre, self.id = name, genre, id
         
-    def get_name(self): return self._name
-    def get_albums(self): return self._albums
-    def get_genre(self): return self._genre
+    def create_table(cls):
+        """Create the table for the Band model"""
+        sql = """CREATE TABLE IF NOT EXISTS bands (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                genre TEXT
+            )"""
+        CURSOR.execute(sql)
+        CONN.commit()
+        
+    def drop_table(cls):
+        """Drop the table for the Band model"""
+        sql = """DROP TABLE IF EXISTS bands"""
+        CURSOR.execute(sql)
+        CONN.commit()
+        
+    def save(self):
+        """Save the band to the database"""
+        sql = """INSERT INTO bands (name, genre) VALUES (?, ?)"""
+        CURSOR.execute(sql, (self.name, self.genre))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
+        
+    def create(cls, name, genre):
+        """Create a new band"""
+        band = cls(name, genre)
+        band.save()
+        return band
     
-    def set_name(self, name):
-        self._name = name
-    def set_albums(self, albums):
-        self._albums = albums
-    def set_genre(self, genre):
-        self._genre = genre
+    def update(self):
+        """Update the band in the database"""
+        sql = """UPDATE bands SET name = ?, genre = ? WHERE id = ?"""
+        CURSOR.execute(sql, (self.name, self.genre, self.id))
+        CONN.commit()
         
-    name = property(get_name, set_name)
-    albums = property(get_albums, set_albums)
-    genre = property(get_genre, set_genre)
+    def delete(self):
+        """Delete the band from the database"""
+        sql = """DELETE FROM bands WHERE id = ?"""
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
+        del type(self).all[self.id]
+        self.id = None
+        
+    def find_by_id(cls, id):
+        """Find a band by id"""
+        sql = """SELECT * FROM bands WHERE id = ?"""
+        CURSOR.execute(sql, (id,))
+        row = CURSOR.fetchone()
+        return cls.instance_from_db(row) if row else None
+    
+    def instance_from_db(cls, row):
+        """Return a band instance from a database row"""
+        band = cls.all.get(row[0])
+        if band:
+            band.name = row[1]
+            band.genre = row[2]
+        else:
+            band = cls(row[1], row[2])
+            band.id = row[0]
+            cls.all[band.id] = band
+        
+    def get_all(cls):
+        sql = """SELECT * FROM bands"""
+        
+        rows = CURSOR.execute(sql).fetchall()
+        return [cls.instance_from_db(row) for row in rows]
